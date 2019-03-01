b39f9b9dbe6d6bb65315e1cbe79a17b63ebf2010
sunzid02 <sunzid02@gmail.com>
2019-03-01 13:29:12 +0600

    chat box designed with input chat

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -7,22 +7,27 @@
     <title>CHAT</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <style>
+         .list-group{
+            overflow-y: scroll; 
+            height: 200px; 
+         }
+    </style>
 </head>
 <body>
 
         <div class="container">
-        <br>
             <div class="row" id="app">
-                <ul class="list-group offset-4 col-4">
+                <div class="offset-4 col-4">
                     <li class="list-group-item active">Chat ROOM</li>
-                  
-                    <message v-for="value in chat.message">
-                     @{{ value }}
-                    </message>
-
-                    <input type="text" name="" id="" class='form-control' placeholder='Type your message here..' 
-                       v-model='message' @keyup.enter='send'>
-                </ul>
+                    <ul class="list-group">
+                        <message v-for="value in chat.message">
+                            @{{ value }}
+                        </message>
+                    </ul>
+                        <input type="text" name="" id="" class='form-control' placeholder='Type your message here..' 
+                        v-model='message' @keyup.enter='send'>
+                </div>
             </div>
         </div>
 
