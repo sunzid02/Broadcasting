bd608eb42598341630e94afae73920e612af72ad
sunzid02 <sunzid02@gmail.com>
2019-03-01 14:27:24 +0600

    Chat event, controller, broadcast added

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('chat', function () {
-    return view('chat');
-});
+Route::get('chat', 'ChatController@chat');
 
 
 // Route::get('/event', function () {
